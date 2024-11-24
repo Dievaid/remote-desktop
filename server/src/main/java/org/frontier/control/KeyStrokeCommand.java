0cf61421db548283c200de09479d29eaa3cf8e83
David Capragiu <dievaid@Davids-MacBook-Pro.local>
2024-11-25 00:27:07 +0200

    Handle key combinations

--- server/src/main/java/org/frontier/control/KeyStrokeCommand.java
+++ server/src/main/java/org/frontier/control/KeyStrokeCommand.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.List;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -13,11 +15,21 @@ public class KeyStrokeCommand implements Command {
     private final DataInputStream dis;
     private final Robot robot;
 
+    private static final java.util.List<Integer> combinationKeyCodes = List.of(
+            KeyEvent.VK_SHIFT, KeyEvent.VK_CONTROL, KeyEvent.VK_ALT);
+
     @Override
     public void execute() {
         try {
             int keyCode = dis.readInt();
             robot.keyPress(keyCode);
+
+            if (combinationKeyCodes.contains(keyCode)) {
+                int combinationKeyCode = dis.readInt();
+                robot.keyPress(combinationKeyCode);
+                robot.keyRelease(combinationKeyCode);
+            }
+
             robot.keyRelease(keyCode);
         } catch (IOException e) {
             log.error("Error reading key stroke command", e);
